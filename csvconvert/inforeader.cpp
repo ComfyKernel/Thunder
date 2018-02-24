074756633d60407cca1d536169a4ec29977d15c6
comfykernel <comfykernel@gmail.com>
2018-02-24 11:32:18 -0800

    Holy Cr*p it worked first try

--- csvconvert/inforeader.cpp
+++ csvconvert/inforeader.cpp
@@ -78,6 +78,38 @@ bool inforeader::open(const std::string& filename) {
       std::cout<<"[inforeader] Found '"<<name<<
 	"' of type '"<<type<<
 	"' with value '"<<value<<"'\n";
+      
+      if(type == "uint") {
+	unsigned int* val = new unsigned int(std::stoul(value, nullptr, 0));
+	
+	_element_names.push_back(name);
+	_element_values.push_back(val);
+      } else if(type == "list") {
+	bool found = false;
+	
+	for(int i=0; i<_element_names.size(); ++i) {
+	  if(_element_names[i] == name) {
+	    found = true;
+
+	    std::vector<std::string>* vec = (std::vector<std::string>*)_element_values[i];
+
+	    vec->push_back(value);
+	    
+	    break;
+	  }
+	}
+
+	if(!found) {
+	  _element_names.push_back (name);
+	  _element_values.push_back(new std::vector<std::string>);
+	  ((std::vector<std::string>*)_element_values[_element_values.size() - 1])->push_back(value);
+	}
+      } else {
+	std::cout<<"Unknown type for element '"<<name<<"' Defaulting to string\n";
+
+	_element_names.push_back (name);
+	_element_values.push_back(new std::string(val));
+      }
     }
     
     if(iscom_type) {
