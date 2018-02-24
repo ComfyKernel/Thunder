0687344f9d5e223b2922f05a8f1a9b0e230a24b6
comfykernel <comfykernel@gmail.com>
2018-02-23 18:50:31 -0800

    Started work on maps and stuff

---
+++ tester/Thunder-Test Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Thunder-Test Tiles" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="1024" columns="32">
+ <image source="test-tileset.png" width="576" height="576"/>
+</tileset>
