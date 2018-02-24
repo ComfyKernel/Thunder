912e6a150abab4fa33c19c1d1ff50dea8480ac7f
comfykernel <comfykernel@gmail.com>
2018-02-24 11:11:46 -0800

    InfoReader is getting there

--- csvconvert/inforeader.cpp
+++ csvconvert/inforeader.cpp
@@ -17,6 +17,78 @@ bool inforeader::open(const std::string& filename) {
     return false;
   }
 
+  std::string word;
+  std::string type;
+  std::string name;
+  std::string value;
+  while(fi >> word) {
+    bool iscom_type = false;
+    bool iscom_name = false;
+
+    bool isval      = true;
+    bool isstring   = false;
+    
+    std::string val = "";
+    
+    for(const auto& c : word) {
+      if(!isstring) {
+	switch(c) {
+	case '(':
+	  iscom_type = true;
+	  isval      = false;
+	  break;
+	case ')':
+	  if(iscom_type) {
+	    type = val;
+
+	    std::cout<<"[inforeader] Setting type to '"<<type<<"'\n";
+	  }
+	  break;
+	case '[':
+	  iscom_name = true;
+	  isval      = false;
+	  break;
+	case ']':
+	  if(iscom_name) {
+	    name = val;
+	  }
+	  break;
+	case '\'':
+	  isstring = true;
+	  break;
+	default:
+	  val += c;
+	  break;
+	}
+      } else {
+	switch(c) {
+	case '\'':
+	  isstring = false;
+	  break;
+	default:
+	  val += c;
+	  break;
+	}
+      }
+    }
+
+    if(isval) {
+      value = val;
+      
+      std::cout<<"[inforeader] Found '"<<name<<
+	"' of type '"<<type<<
+	"' with value '"<<value<<"'\n";
+    }
+    
+    if(iscom_type) {
+      type = val;
+    }
+    
+    if(iscom_name) {
+      name = val;
+    }
+  }
+
   return true;
 }
 
